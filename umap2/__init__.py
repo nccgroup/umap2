6b1e9094249e9541e52e9ca20c244c031b775054
Binyamin Sharet <s.binyamin@gmail.com>
2016-08-29 23:57:48 +0300

    [Logger] moved logger code to ulogger, some reformatting

--- umap2/__init__.py
+++ umap2/__init__.py
@@ -1,25 +1,7 @@
 #
 # Add debug levels/functions
 #
-import logging
-
-
-def prepare_logging():
-    def add_debug_level(num, name):
-        def fn(self, message, *args, **kwargs):
-            if self.isEnabledFor(num):
-                self._log(num, message, args, **kwargs)
-        logging.addLevelName(num, name)
-        setattr(logging, name, num)
-        return fn
-
-    logging.Logger.verbose = add_debug_level(5, 'VERBOSE')
-    logging.Logger.always = add_debug_level(100, 'ALWAYS')
-
-    FORMAT = '[%(levelname)-6s] %(message)s'
-    logging.basicConfig(format=FORMAT)
-    logger = logging.getLogger('umap2')
-    logger.setLevel(logging.INFO)
+from umap2.utils.ulogger import prepare_logging
 
 
 prepare_logging()
