6ace3c07f54437f24118e95c259e8bc96847a7b2
Binyamin Sharet <s.binyamin@gmail.com>
2016-08-31 22:28:35 +0300

    [Apps] Added more information to builtin devices in umap2list and in base app

--- umap2/apps/list_classes.py
+++ umap2/apps/list_classes.py
@@ -3,7 +3,10 @@
 List default classes supported in umap
 
 Usage:
-    umap2list
+    umap2list [-v]
+
+Options:
+    -v, --verbose   show more information
 '''
 from umap2.apps.base import Umap2App
 
@@ -12,8 +15,15 @@ class Umap2ListClassesApp(Umap2App):
 
     def run(self):
         ks = self.umap_classes
+        verbose = self.options.get('--verbose', False)
+        if verbose:
+            print '%-20s  %s' % ('Device', 'Description')
+            print '--------------------  ----------------------------------------------------'
         for k in ks:
-            print('%s' % k)
+            if verbose:
+                print('%-20s  %s' % (k, self.umap_class_dict[k][1]))
+            else:
+                print('%s' % k)
 
 
 def main():
